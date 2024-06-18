70a4a59236c335e7982fbcbda800ed0aaa2ee83e
Vinicius Paes Berna <vini_berna2.0@hotmail.com>
2024-06-18 14:19:03 -0300

    feat: add more example of the fib function

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,4 +1,4 @@
-// should return the value of a position from fibonacci sequentice
+// should return the value of a position from fibonacci sequence
 // 1, 1, 2, 3, 5, 8, 13...
 function fib(n) {
   if (n <= 2) return 1
@@ -6,4 +6,11 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2)
 }
 
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(5))
+console.log(fib(6))
 console.log(fib(7))
+console.log(fib(8))
